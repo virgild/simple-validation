fe9220e613776337572bea188e5a164afa68f91e
Virgil Dimaguila <virgil@roundysoft.com>
2009-08-31 18:58:14 -0400

    Updated DSL and tests

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -8,25 +8,60 @@ describe Validation do
       def initialize
       end
     end
+    
+    @user_validation = Validation.new do |v|
+      v.complain "Username should be provided", :about => :username, :as => :username_not_blank, :when => Proc.new { |s| s.username.blank? }
+      v.complain "Username should be at least 5 characters", :about => :username, :as => :username_min_length, :when => Proc.new { |s| s.username.length < 5 }
+      v.complain "E-mail should be provided", :about => :email, :as => :email_not_blank, :when => Proc.new { |s| s.email.blank? }
+      v.complain "E-mail should end with @local", :about => :email, :as => :email_domain, :when => Proc.new { |s| s.email.grep(/\@local$/).length == 0 }
+      v.depends(:username_min_length, :username_not_blank)
+      v.depends(:email_domain, :email_not_blank)
+    end
   end
 
-  it "should build validations" do
-    user_validation = Validation.new do |v|
-      v.complain "Username should be provided", :about => :username, 
-        :when => Proc.new { |subject| subject.username.blank? }
-      v.complain "Username should be at least 5 characters", :about => :username, 
-        :when => Proc.new { |subject| !subject.username.blank? && subject.username.length < 5 }
-    end
-    
+  it "find nodes" do
+    @user_validation.find_check_node(:email_not_blank).name.should == :email_not_blank    
+  end
+  
+  it "should be constructed" do
+    @user_validation.checks.should have(2).children
+    @user_validation.checks[:username_not_blank].should have(1).children
+    @user_validation.checks[:username_not_blank][:username_min_length].should_not be_nil
+    @user_validation.checks[:email_not_blank].should have(1).children
+    @user_validation.checks[:email_not_blank][:email_domain].should_not be_nil
+  end
+  
+  it "should validate object" do
     user = User.new
-    user.username = ""
     
-    user_validation.run(user)
-    user_validation.should have(1).complaints
+    result = @user_validation.run(user)
+    result.length.should == 2
+    result[0][:about].should == :username
+    result[0][:message].should == "Username should be provided"
+    result[1][:about].should == :email
+    result[1][:message].should == "E-mail should be provided"
     
-    complaint = user_validation.complaints[0]
-    complaint[:about].should == :username
-    complaint[:message].should == "Username should be provided"
+    user.username = "test"
+    result = @user_validation.run(user)
+    result.length.should == 2
+    result[0][:about].should == :username
+    result[0][:message].should == "Username should be at least 5 characters"
+    result[1][:about].should == :email
+    result[1][:message].should == "E-mail should be provided"
+    
+    user.username = "tester"
+    result = @user_validation.run(user)
+    result.length.should == 1
+    result[0][:about].should == :email
+    result[0][:message].should == "E-mail should be provided"
+    
+    user.email = "wrong@email.com"
+    result = @user_validation.run(user)
+    result.length.should == 1
+    
+    user.email = "tester@local"
+    result = @user_validation.run(user)
+    result.length.should == 0
   end
 
 end
--- validation.rb
+++ validation.rb
@@ -1,33 +1,60 @@
 require 'extlib'
 
+begin
+  require 'tree'
+rescue LoadError
+  $stderr.puts "Validation requires the rubytree library"
+end
+
 class Validation
-  attr_reader :checks, :complaints
+  attr_reader :checks
   
   def initialize(&block)
-    @checks = Array.new
-    @complaints = Array.new
-    yield self
+    @checks = Tree::TreeNode.new(:root)
+    block.call(self) if block
   end
   
   def complain(message, options)
-    @checks << { 
-      :message => message, 
-      :when => options[:when],
-      :about => options[:about]
-    }
+    spec = { :message => message, :as => options[:as], :about => options[:about], :inspector => options[:when] }
+    @checks << Tree::TreeNode.new(options[:as], spec)
+    self
+  end
+  
+  def depends(dependent_check, parent_check)
+    dependent_node = find_check_node(dependent_check)
+    parent_node = find_check_node(parent_check)
+    dependent_node.removeFromParent!
+    parent_node << dependent_node
   end
   
   def run(subject)
-    @complaints.clear
-    @checks.each do |check|
-      proc = check[:when]
-      if proc.call(subject)
-        @complaints << {
-          :about => check[:about],
-          :message => check[:message]
-        }
+    complaints = Array.new
+    @checks.children.each do |node|
+      complaints << run_node(node, subject)
+    end
+    
+    complaints.flatten
+  end
+  
+  def find_check_node(name)
+    @checks.find { |node| node.name == name }
+  end
+  
+  private
+  
+  def run_node(check_node, subject)
+    check = check_node.content
+    complaints = Array.new
+    
+    if check[:inspector].call(subject)
+      complaints << { :message => check[:message], :about => check[:about] }
+    else
+      check_node.children.each do |node|
+        complaints << run_node(node, subject)
       end
     end
+    
+    complaints.flatten
   end
-
+  
 end
